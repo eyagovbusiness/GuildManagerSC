adbf170a78368155117da5587717e3ee5ced71a1
Eduardo Yago Vicent <eyagovbusiness@gmail.com>
2023-04-16 21:28:52 +0200

    Updated RabbitMQ infrastructure.

--- Infrastructure/RabbitMQ/rabbitmq_init.sh
+++ Infrastructure/RabbitMQ/rabbitmq_init.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
+echo "RabbitMQ initialization script started."
+
+if [ "$ASPNETCORE_ENVIRONMENT" = "Development" ]; then
+	CreateUserEcho="*** Administrator user '$RABBITMQ_USER' with password '$RABBITMQ_PASSWORD' created successfully! ***"
+else
+	CreateUserEcho="Administrator user created successfully!" 
+fi
+
 # Create Rabbitmq user
 ( rabbitmqctl wait --timeout 60 $RABBITMQ_PID_FILE ; \
 rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD 2>/dev/null ; \
 rabbitmqctl set_user_tags $RABBITMQ_USER administrator ; \
 rabbitmqctl set_permissions -p / $RABBITMQ_USER  ".*" ".*" ".*" ; \
-echo "*** User '$RABBITMQ_USER' with password '$RABBITMQ_PASSWORD' completed. ***" ; \
-echo "*** Log in the WebUI at port 15672 (example: http:/localhost:15672) ***") &
+echo $CreateUserEcho ) &
 
 # $@ is used to pass arguments to the rabbitmq-server command.
 # For example if you use it like this: docker run -d rabbitmq arg1 arg2,
 # it will be as you run in the container rabbitmq-server arg1 arg2
 rabbitmq-server $@
+
+echo "RabbitMQ initialization script finished."
