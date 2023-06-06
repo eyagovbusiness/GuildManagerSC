ae88d0a33e92cb80151d74e11278c67868d8fbd8
Eduardo Yago Vicent <eyagovbusiness@gmail.com>
2023-06-06 20:50:43 +0200

    -Added mysql service to the docker-compose. -MySql service registered in consul init script.

--- Infrastructure/Consul/consul_init.sh
+++ Infrastructure/Consul/consul_init.sh
@@ -6,6 +6,7 @@ function consul_init_func(){
 # Infrastructure
 consul services register -name=RabbitMQ -address=rabbitmq
 consul services register -name=VaultSecretsManager -address=http://vault -port=8200
+consul services register -name=MySql -address=http://mysql -port=3306
 
 # Services
 consul services register -name=Mandril -address=http://mandril-ms -port=80
